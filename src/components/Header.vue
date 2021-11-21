61f15d39780f3dc00e939527a6211eec5e1890a2
zgmrvn <zgmrvn@gmail.com>
2021-11-21 18:32:20 +0100

    Remove trianlge

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,5 @@
 <template>
   <header class="flex items-center justify-center bg-gray-50 py-48">
-    <!-- <Triangle /> -->
-
     <h1 class="text-center text-xl">
       <span class="text-5xl font-semibold leading-loose">
         Marvin Saignat
@@ -11,13 +9,3 @@
     </h1>
   </header>
 </template>
-
-<script>
-import Triangle from '@/components/Triangle'
-
-export default{
-  components: {
-    Triangle
-  }
-}
-</script>
